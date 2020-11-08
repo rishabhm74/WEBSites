51db51aa45c66632f2c3571994c1b809e7847634
rishabh <rishabhm1974@gmail.com>
2020-11-08 14:57:10 +0530

    fotostudio init

---
+++ fotoStudio/static/js/script.js
@@ -0,0 +1,17 @@
+$(function() {
+    $(window).scroll(function() {
+        // var windowScrolledDist = $(document).scrollTop();
+        // var navHeight = $('.navbar').height();
+        // var logoHeight = $('.logoContainer').height();
+        // var logoOffsetTop = $('.logoContainer').offset();
+        // console.log(logoHeight, logoOffsetTop.top);
+        // if(windowScrolledDist > (logoHeight + 40)) {
+        //     $('.navbar').addClass('navbarOnScroll');
+        //     $('.featuredContainer').addClass('overlayMoved');
+        // }
+        // if(windowScrolledDist <= (logoHeight + 40)) {
+        //     $('.navbar').removeClass('navbarOnScroll');
+        //     $('.featuredContainer').removeClass('overlayMoved');
+        // }
+    })
+})
